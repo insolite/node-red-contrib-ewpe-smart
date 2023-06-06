7999fc08ffc9ddfc0765591f26aef739cba4d2fa
Oleg Krasnikov <oleg.krasnikov@diversido.io>
2023-06-06 21:23:12 +0300

    Fix node

--- nodes/ewpe-get-state.js
+++ nodes/ewpe-get-state.js
@@ -3,7 +3,7 @@ const { DeviceManager } = require('../ewpe-smart');
 module.exports = (RED) => {
   const deviceManager = new DeviceManager('192.168.24.255'); // FIXME: dynamic config
 
-  const GetStateNode = (config) => {
+  function GetStateNode (config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on('input', (msg) => {
@@ -14,6 +14,9 @@ module.exports = (RED) => {
       deviceManager.getDeviceStatus(this.id)
         .then((state) => {
           node.send(state);
+        })
+        .catch((err) => {
+          console.error(err);
         });
       // msg.payload = msg.payload.toLowerCase();
       // node.send(msg);
